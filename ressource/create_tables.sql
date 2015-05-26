99f2abaef07874030ea287e36776fa2c519b7d70
Ashen-ker <corentin.gigayoga@hotmail.fr>
2015-05-26 11:10:46 +0200

    create_tables.sql avec 2 utilisateurs

--- ressource/create_tables.sql
+++ ressource/create_tables.sql
@@ -1,8 +1,49 @@
-CREATE TABLE user (
-	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	login VARCHAR(30) NOT NULL,
-	pwd VARCHAR(30) NOT NULL,
-	surname VARCHAR(30) NOT NULL,
-	lastname VARCHAR(30) NOT NULL,
-	age INT(3) NOT NULL
-)
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 26 Mai 2015 à 11:09
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `cookme`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(30) NOT NULL,
+  `pwd` varchar(30) NOT NULL,
+  `surname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `age` int(3) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `pwd`, `surname`, `lastname`, `age`) VALUES
+(1, 'admin', 'password', 'admin', 'admin', 31),
+(2, 'user', 'password', 'user', 'user', 40);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
