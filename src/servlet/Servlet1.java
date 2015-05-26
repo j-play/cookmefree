658a8d0171096b858a8dcfba36d8e59367bf57dc
Ashen-ker <corentin.gigayoga@hotmail.fr>
2015-05-26 11:20:15 +0200

    TODO completed but not tested in Servlet1.java

--- src/servlet/Servlet1.java
+++ src/servlet/Servlet1.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.UserModel;
 import db.DB;
 
 /**
@@ -29,8 +31,13 @@ public class Servlet1 extends HttpServlet {
 	/**
 	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response) */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	       //TODO Lors de l’appel de http get utiliser les classes précédement créer pour récupérer la liste des utilisateurs
-	       //TODO écrire la liste des utilisateurs dans le flux de réponse HttpServletResponse
+		//TODO Lors de lâ€™appel de http get utiliser les classes preÌ�ceÌ�dement creÌ�er pour reÌ�cupeÌ�rer la liste des utilisateurs
+		ArrayList<UserModel> listUser = db.getData();
+		//TODO eÌ�crire la liste des utilisateurs dans le flux de reÌ�ponse HttpServletResponse
+		for(UserModel i : listUser){
+			response.getWriter().print("prenom "+i.getSurname()+"\nnom "+i.getLastname()+"\nlogin "+i.getLogin()+"\npassword "+i.getPwd()+"\nage "+i.getAge()+"\n\n");
+		}
+		
 	}
 	/**
 	* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response) */
